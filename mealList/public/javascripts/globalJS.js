3964380b236b7400f54b1848e6ee55dddf0fc21b
Jason Runzer <jason.runzer@uoit.net>
2016-11-06 18:40:21 -0800

    adds to todoist

--- mealList/public/javascripts/globalJS.js
+++ mealList/public/javascripts/globalJS.js
@@ -21,15 +21,36 @@ $(document).ready(function() {
 			success: function(res) {
 				var aRecipe = res.hits[0].recipe;
 				for (var i = 0; i < res.hits.length; i++) {
-					$('#meals').append(
-						'<div class="mealItem">' +
-							'<div class="mealLabel">' +
-								'<h3>' + res.hits[i].recipe.label + '</h3>' +
+					var content = '' +
+						'<div class="mealItem col-md-6">' +
+							'<div class="mealBorder">' +
+								'<div class="mealLabel">' +
+									'<a href="' + res.hits[i].recipe.url + '" target="_blank">' +
+										'<h3>' + res.hits[i].recipe.label + '</h3>' +
+									'</a>' +
+								'</div>' +
+								'<div class="mealContents">' + 
+									'<div class="someImage">' +
+										'<a target="_blank" class="foodImage" href="' + res.hits[i].recipe.url + '">' +
+											'<img src="' + res.hits[i].recipe.image + '" target="_blank">' +
+										'</a>' +
+									'</div>' +
+									'<div class="someLabel">' +
+										'<label class="description">' +
+											'<ul id="ul' + i + '">';
+											for (var j = 0; j < res.hits[i].recipe.ingredients.length; j++) {
+												content += '<li>' + res.hits[i].recipe.ingredients[j].text + '</li>';
+											}
+								content += '</ul>' +
+											'<br>' +
+											'<input type="checkbox" value="add" id="' + 'checkbox' + i +'"">' +
+											'<label> Add All ingredients </label>'
+										'</label>' +
+									'</div>' +
+								'</div>' + 
 							'</div>' +
-							'<div class="foodImage">' + 
-								'<img src="' + res.hits[i].recipe.image + '">' +
-							'</div>' + 
-						'</div>');
+						'</div>';
+					$('#meals .row').append(content);
 				}
 			},
 			error: function(res) {
@@ -38,6 +59,69 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+	$('#add').click(function() {
+		var email = $('#email').val();
+		var passwordValue = $('#password').val();
+		var listNameValue = $('#ListName').val();
+		alert(email + " " + password + " " + listNameValue);
+		var list = $('#meals .row').children();
+		alert(list.length);
+		var mealsToAdd = [];
+		for (var i = 0; i < list.length; i++) {
+			if($('#checkbox' + i).is(':checked')) {
+				mealsToAdd.push(i);
+			}
+		}
+		var ingredients = [];
+		for(var i = 0; i < mealsToAdd.length; i++) {
+			var list = $('#ul' + mealsToAdd[i]).children();
+			for(var j = 0; j < list.length; j++) {
+				ingredients.push({name: list.eq(j).text()});
+			}			
+		}
+		
+		for (var i = 0; i < ingredients.length; i++) {
+			alert(ingredients[i].name);
+		}
+
+		//send ajax post with ingredients and shopping list name
+		/* POST todoist. - makes a list with the items
+		  {
+		    "username": "username@email.com",
+		    "password": "password",
+		    "listname": "Shopping List",
+		    "items": [{
+		      "name": "carrots"
+		    }, {
+		      "name": "peas"
+		    }]
+		  } 
+		*/
+		var listData = {
+						username: email,
+						passwrd: passwordValue,
+						listname: listNameValue,
+						items: ingredients
+						};
+
+		$.ajax({
+			type: 'POST',
+			url: '/todoist',
+			data: JSON.stringify(listData),
+			dataType: 'JSON',
+			contentType: "application/json; charset=utf-8",
+			success: function(res) {
+				alert('success');
+				
+			},
+			error: function(res) {
+				console.log(res);
+				alert("error posting list");
+			}
+		});
+	});
+
 });
 
 
