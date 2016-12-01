a0ab1ccd197165d595c3d1159cf02584335eb253
Jason Runzer <jason.runzer@uoit.net>
2016-11-30 18:56:34 -0800

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install nodejs
+ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get install python
+sudo apt-get install npm
+pip install todoist-python
