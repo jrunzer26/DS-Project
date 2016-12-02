c1b33ec67596f10f32d912ea704d80a3a1aaebed
jrunzer26 <jason.runzer@uoit.net>
2016-12-02 07:24:56 -0800

    updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-sudo apt-get install nodejs
+sudo apt-get install build-essential checkinstall nodejs python npm python-pip libzmq-dev
 ln -s /usr/bin/nodejs /usr/bin/node
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-sudo apt-get install python
-sudo apt-get install npm
 pip install todoist-python
+pip install zerorpc
+
